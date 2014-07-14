406d16477754c6af75fdb896c1e224f0b1e183aa
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2014-07-14 09:24:11 -0500

    Incorporate feedback from scymtym on sb-unicode-strings

--- contrib/sb-unicode-strings/base-type.lisp
+++ contrib/sb-unicode-strings/base-type.lisp
@@ -2,14 +2,24 @@
 
 ;;; Class definition
 (defclass unicode-string (sequence standard-object)
-  ((%vector :initarg :vector :accessor %vector)))
+  ;; Vector of strings (grapheme clusters)
+  ((%vector :initarg :vector :accessor %vector))
+  (:default-initargs :vector (sb-int:missing-arg)))
 
+(defun %make-backing-array (length &rest args &key initial-element initial-contents)
+  (declare (ignore initial-element initial-contents))
+  (apply #'make-array length :element-type 'string :fill-pointer 0 args))
+
+(declaim (ftype (function (string) unicode-string)
+                string->unicode-string))
 (defun string->unicode-string (string)
   "Converts STRING to a Unicode string"
   (make-instance
    'unicode-string
    :vector (graphemes (normalize-string string) :as-vector t)))
 
+(declaim (ftype (function (unicode-string) string)
+                string->unicode-string))
 (defun unicode-string->string (uni-string)
   "Converts UNI-STRING to a ANSI Common LIsp string"
   (let ((ret (make-array (length uni-string) :element-type 'character
@@ -21,34 +31,34 @@
 
 ;;; Sequence protocal
 (defmethod sb-sequence:length ((str unicode-string))
-  (sb-sequence:length (%vector str)))
+  (length (%vector str)))
 
 (defmethod sb-sequence:elt ((str unicode-string) index)
   (aref (%vector str) index))
 
 (defmethod (setf sb-sequence:elt) ((new-value character) (str unicode-string) index)
-  (setf (aref (%vector str) index) (normalize-string (coerce (list new-value) 'string))))
+  (setf (aref (%vector str) index) (normalize-string (string new-value))))
 
 (defmethod (setf sb-sequence:elt) ((new-value string) (str unicode-string) index)
-  (let ((value (graphemes (normalize-string new-value))))
-    (when (cdr value) (error "New value ~s contains more than one grapheme" new-value))
-    (setf (aref (%vector str) index) (car value))))
+  (let ((graphemes (graphemes (normalize-string new-value))))
+    (when (rest graphemes)
+      (error "New value ~s contains more than one grapheme" new-value))
+    (setf (aref (%vector str) index) (first graphemes))))
 
 (defmethod (setf sb-sequence:elt) (new-value (str unicode-string) index)
   (error 'type-error :expected-type '(or string character) :datum new-value))
 
-(defmethod sb-sequence:adjust-sequence ((str unicode-string) length &rest args &key initial-element initial-contents)
+(defmethod sb-sequence:adjust-sequence ((str unicode-string) length &rest args
+                                        &key initial-element initial-contents)
   (declare (ignore initial-element initial-contents))
-  (setf
-   (%vector str)
-   (apply #'sb-sequence:adjust-sequence (%vector str) length args)))
+  (setf (%vector str) (apply #'adjust-array (%vector str) length args))
+  str)
 
-(defmethod sb-sequence:make-sequence-like ((str unicode-string) length &rest args &key initial-element initial-contents)
+(defmethod sb-sequence:make-sequence-like ((str unicode-string) length &rest args
+                                           &key initial-element initial-contents)
   (declare (ignore initial-element initial-contents))
-  (make-instance
-   'unicode-string
-   :vector
-   (apply #'sb-sequence:make-sequence-like (%vector str) length args)))
+  (make-instance 'unicode-string :vector
+                 (apply #'%make-backing-array length args)))
 
 (defmethod sb-sequence:canonize-test ((s unicode-string) test test-not)
   (declare (ignore s))
@@ -58,9 +68,11 @@
                   (complement test-not)
                   (complement (fdefinition test-not))))
     (t #'(lambda (x y)
-           (when (characterp x) (setf x (coerce (list x) 'string)))
-           (when (characterp y) (setf y (coerce (list y) 'string)))
-           (string= (normalize-string x) (normalize-string y))))))
+           (string=
+            ;; If x or y are already strings, STRING does nothing.
+            ;; If they're characters, makes them singleton strings.
+            (normalize-string (string x))
+            (normalize-string (string y)))))))
 
 
 ;;; Input and output
